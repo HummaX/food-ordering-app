550f36ebc12283d3744537d992d9530e9100ba84
HummaX <hummas_ch@hotmail.com>
2023-04-26 23:59:22 +0500

    added firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,47 +1,82 @@
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem';
-
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+import { useEffect, useState } from 'react';
 
 let AvailableMeals = ()=>{
 
-let mealsList = DUMMY_MEALS.map((meals => <MealItem key={meals.id} name={meals.name} description={meals.description} price={meals.price} id={meals.id}/>))
+  let [meals,setMeals] = useState([])
+  let [loading,setLoading] = useState(true)
+  let [error,setError] = useState('')
 // we can pass meals in props too and can access it via props.meals.name
+
+useEffect(()=>{
+  let mealListFetcher = async()=>{
+  let response = await fetch('https://react-api-project-e7084-default-rtdb.asia-southeast2.firebasedatabase.app/mealsList.json')
+  if(!response.ok){
+    console.log('error response')
+  }
+  let data = await response.json()
+  
+  let loadedMeals = []
+
+  for(let fetchedData in data){
+   loadedMeals.push(data[fetchedData])
+  //  loadedMeals.push({
+  //   id:data[fetchedData].id,
+  //   description:data[fetchedData].description,
+  //   name:data[fetchedData].name
+  //  })
+  }
+  setMeals(loadedMeals)
+  // When done getting data put loading to false
+  setLoading(false)
+  }
+  // since mealsListFetcher is a async function we need to use await by wraping it in try catch, but await wont work here
+  // as it will beark promis we can return a new fucntion where we can catch error
+  mealListFetcher().catch((error)=>{
+    setError(error.message)
+    setLoading(false)
+  })
+
+}, [])
+
+let mealsgot = meals.map((meals => <MealItem key={meals.id} name={meals.name} description={meals.description} price={meals.price} id={meals.id}/>))
+  
+if(loading){
+  console.log('loading')
+  return(  
+    <section className={classes.mealsLoading}>
+      <Card>
+      <h1>
+        Loading...
+      </h1>
+      </Card>
+    </section>
+
+  )
+}
+
+if(error){
+  console.log('Error')
+  return(  
+    <section className={classes.mealsLoading}>
+      <Card>
+      <h1>
+        {error}
+      </h1>
+      </Card>
+    </section>
+
+  )
+}
 
 return(
 <>
     <section className={classes.meals}>
         <Card>
-        {/* <MealItem/> */}
-        {mealsList}
+        {mealsgot}
         </Card>
-
     </section>
 
 </>
